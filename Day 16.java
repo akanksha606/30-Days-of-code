5c5838228fcc37620b96217b957ebdd8118c3eab
akanksha606 <akankshagupta98583@gmail.com>
2020-07-26 20:08:55 +0530

    Day 16

---
+++ Day 16.java
@@ -0,0 +1,22 @@
+//Day 16(Exceptions - String to Integer)
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        int i=0;
+        try{
+            i=Integer.parseInt(S);
+            System.out.print(i);
+        }
+        catch(Exception e){
+            System.out.print("Bad String");
+        }
+    }
+}
